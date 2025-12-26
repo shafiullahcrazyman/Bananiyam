6dddd47ce237ebc424cd587729337bd3b5920aa8
Shafiullah <shafiullahcrazyman@gmail.com>
2025-12-26 07:33:59 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
+import App from '../App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -11,7 +11,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {/* HashRouter handles the URL routing on GitHub Pages preventing 404s */}
     <HashRouter>
       <App />
     </HashRouter>
